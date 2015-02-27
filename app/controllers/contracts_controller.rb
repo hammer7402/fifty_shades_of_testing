e614d6958e79a317fb86f5e08deb14f101d08b7c
Michael Campbell <MCcool322@aol.com>
2015-02-27 09:55:26 -0500

    first pass test

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -1,4 +1,14 @@
 class ContractsController < ApplicationController
+
+  def index
+
+  end
+
   def new
+
+  end
+
+  def show
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
+  root 'contracts#index'
 
+  resource :contract
 
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
 end
